286577f1bdf7524a8a2e7e6d6e865580dcfab18c
Saleem Mukhtar <ateeqfarman65@gmail.com>
2024-10-31 15:41:30 +0000

    update data load

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -1,4 +1,13 @@
+# import pandas as pd
+# def load_titanic_data(filepath: str) -> pd.DataFrame:
+#     df = pd.read_csv(filepath)
+#     return df
+
 import pandas as pd
 def load_titanic_data(filepath: str) -> pd.DataFrame:
     df = pd.read_csv(filepath)
     return df
+    if __name__ == "__main__":
+        filepath = "data/titanic.csv"
+        titanic_data = load_titanic_data(filepath)
+        
